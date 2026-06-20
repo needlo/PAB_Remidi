23c781b37631f7d7612bba2c364f7aa29dd63486
needlo <145987439+needlo@users.noreply.github.com>
2026-06-21 00:33:48 +0700

    Delete remidi/ab directory

--- remidi/ab/android/app/src/main/kotlin/com/example/spacenews_core/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.spacenews_core
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- remidi/ab/test/widget_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('SpaceNews Core project loads', () {
-    expect(true, isTrue);
-  });
-}
